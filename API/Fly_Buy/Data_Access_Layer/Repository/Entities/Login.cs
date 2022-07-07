7e8fdf3290eb1498aba1d042c51ecdfc7ab07fff
Amoda <ap.weragoda@gmail.com>
2022-07-07 22:28:29 +0530

    Add project files.

---
+++ API/Fly_Buy/Data_Access_Layer/Repository/Entities/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Access_Layer.Repository.Entities
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
